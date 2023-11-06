37b40359a10c5eaedf14d2ca9a034e1b873aa033
İlker Civil <ilkercivil@gmail.com>
2023-11-06 11:23:47 +0300

    yeni io

--- main.py
+++ main.py
@@ -1,40 +1,71 @@
-import tkinter as tk
-from tkinter import filedialog
-from tkinter import Toplevel
-import pandas as pd
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QVBoxLayout, QLabel, QLineEdit, QPushButton, QScrollArea, QFormLayout
 
-def veri_sec():
-    dosya_path = filedialog.askopenfilename(filetypes=[("Excel Dosyaları", "*.xlsx")])
-    if dosya_path:
-        # Excel dosyasını oku ve verileri sakla
-        global veriler  # Verilere erişebilmek için global olarak tanımlıyoruz
-        veriler = pd.read_excel(dosya_path)
-        print("Seçilen Excel dosyası yolu:", dosya_path)
-        print("Excel verileri:")
-        print(veriler)
-        hesaplamalara_basla_dugme.config(state=tk.NORMAL)  # "Hesaplamalara Başla" düğmesini etkinleştir
+class VeriGirmeEkrani(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Veri Giriş Ekranı")
+        self.setGeometry(100, 100, 800, 600)  # Pencere boyutunu ayarlayabilirsiniz
 
-def hesaplamalara_basla():
-    # Hesaplamaları burada yapabilirsiniz
-    hesaplamalar_penceresi = Toplevel(root)
-    hesaplamalar_penceresi.title("Hesaplamalar")
-    hesaplamalar_penceresi.geometry("600x400")  # Yeni pencerenin boyutunu 600x400 olarak ayarladık
-    # Hesaplamaları bu yeni pencerede gösterebilirsiniz
+        self.layout = QVBoxLayout()
+        self.scroll_area = QScrollArea()
+        self.scroll_area.setWidgetResizable(True)
+        self.scroll_widget = QWidget()
+        self.scroll_area.setWidget(self.scroll_widget)
+        self.layout.addWidget(self.scroll_area)
 
-# Ana tkinter penceresini oluştur
-root = tk.Tk()
-root.title("Excel Veri Seçme Aracı")
+        self.form_layout = QFormLayout()
+        self.scroll_widget.setLayout(self.form_layout)
 
-# Başlangıç ekranını küçültme
-root.geometry("400x300")  # Örneğin, 400x300 boyutunda bir pencere
+        self.veri_etiketleri = [
+            "Ölçüm Nokta Adı",  # Ölçüm Nokta Adı eklendi
+            "LApeak [dB]", "LAFmax [dB]", "LAeq [dB]", "LAeq Ln [dB] L10", "LAeq Ln [dB] L90",
+            "LCpeak [dB]", "LCFmax [dB]", "LCeq [dB]", "LZpeak [dB]", "LZFmax [dB]",
+            "1/3 Oct LAeq [dB] 20 Hz", "1/3 Oct LAeq [dB] 25 Hz", "1/3 Oct LAeq [dB] 31.5 Hz",
+            "1/3 Oct LAeq [dB] 40 Hz", "1/3 Oct LAeq [dB] 50 Hz", "1/3 Oct LAeq [dB] 63 Hz",
+            "1/3 Oct LAeq [dB] 80 Hz", "1/3 Oct LAeq [dB] 100 Hz", "1/3 Oct LAeq [dB] 125 Hz",
+            "1/3 Oct LAeq [dB] 160 Hz", "1/3 Oct LAeq [dB] 200 Hz", "1/3 Oct LAeq [dB] 250 Hz",
+            "1/3 Oct LAeq [dB] 315 Hz", "1/3 Oct LAeq [dB] 400 Hz", "1/3 Oct LAeq [dB] 500 Hz",
+            "1/3 Oct LAeq [dB] 630 Hz", "1/3 Oct LAeq [dB] 800 Hz", "1/3 Oct LAeq [dB] 1000 Hz",
+            "1/3 Oct LAeq [dB] 1250 Hz", "1/3 Oct LAeq [dB] 1600 Hz", "1/3 Oct LAeq [dB] 2000 Hz",
+            "1/3 Oct LAeq [dB] 2500 Hz", "1/3 Oct LAeq [dB] 3150 Hz", "1/3 Oct LAeq [dB] 4000 Hz",
+            "1/3 Oct LAeq [dB] 5000 Hz", "1/3 Oct LAeq [dB] 6300 Hz", "1/3 Oct LAeq [dB] 8000 Hz",
+            "1/3 Oct LAeq [dB] 10000 Hz", "1/3 Oct LAeq [dB] 12500 Hz", "1/3 Oct LAeq [dB] 16000 Hz",
+            "1/3 Oct LAeq [dB] 20000 Hz", "1/3 Oct LAeq [dB] Total A", "1/3 Oct LAeq [dB] Total C",
+            "1/3 Oct LAeq [dB] Total Z"
+        ]
 
-# "Veri Seç" düğmesini oluştur
-veri_sec_dugme = tk.Button(root, text="Veri Seç", command=veri_sec)
-veri_sec_dugme.pack(pady=20)
+        self.veri_girme_kutulari = {}
 
-# "Hesaplamalara Başla" düğmesini oluştur ve etkisizleştir
-hesaplamalara_basla_dugme = tk.Button(root, text="Hesaplamalara Başla", command=hesaplamalara_basla, state=tk.DISABLED)
-hesaplamalara_basla_dugme.pack(pady=20)
+        for etiket in self.veri_etiketleri:
+            label = QLabel(etiket)
+            entry = QLineEdit()
 
-# Ana döngüyü başlat
-root.mainloop()
+            # Sadece belirli kutucukları küçültün
+            if etiket != "Ölçüm Nokta Adı":
+                entry.setFixedSize(80, 20)  # Genişlik ve yükseklik ayarlarını değiştirebilirsiniz
+
+            self.form_layout.addRow(label, entry)
+            self.veri_girme_kutulari[etiket] = entry
+
+        self.kaydet_dugme = QPushButton("Verileri Kaydet")
+        self.kaydet_dugme.clicked.connect(self.verileri_kaydet)
+
+        self.layout.addWidget(self.kaydet_dugme)
+        self.setLayout(self.layout)
+
+    def verileri_kaydet(self):
+        veriler = {etiket: entry.text() for etiket, entry in self.veri_girme_kutulari.items()}
+
+        # Verileri işleme veya kaydetme işlemlerini burada gerçekleştirebilirsiniz
+
+        # Örnek olarak, verileri yazdıralım:
+        print("Girilen Veriler:")
+        for etiket, deger in veriler.items():
+            print(f"{etiket}: {deger}")
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    window = VeriGirmeEkrani()
+    window.show()
+    sys.exit(app.exec_())
